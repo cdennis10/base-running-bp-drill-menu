989f4b2e331294e11ad895498597c3c8655010a3
cdennis10 <codydennis97@yahoo.com>
2026-09-13 14:07:48 -0700

    Remove scheduled block count from header

--- schedule-config.js
+++ schedule-config.js
@@ -6,7 +6,7 @@
   Weekdays use JavaScript numbering: 0=Sunday, 1=Monday, 3=Wednesday.
 */
 window.DR_SCHEDULE_CONFIG = {
-  version: "2026.09.13.4",
+  version: "2026.09.13.5",
   updatedDate: "2026-09-13",
   timeZone: "America/Santo_Domingo",
   timeZoneLabel: "AST / UTC−4",
